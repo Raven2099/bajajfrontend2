a9e8a726fb45cff4d4218fe1b55d68f9cba1b58c
Raven2099 <136494054+Raven2099@users.noreply.github.com>
2024-08-25 14:00:41 +0530

    Update BfhlForm.js

--- src/BfhlForm.js
+++ src/BfhlForm.js
@@ -37,7 +37,7 @@ const BfhlForm = () => {
     }
 
     try {
-      const backendUrl = 'https://bajaj-backend-5iojtadyc-tejus-css-projects.vercel.app/api/bfhl';
+      const backendUrl = 'https://bajaj-backend-5iojtadyc-tejus-css-projects.vercel.app/bfhl';
 // Replace with your backend URL
 
       const res = await axios.post(backendUrl, JSON.parse(jsonInput));
